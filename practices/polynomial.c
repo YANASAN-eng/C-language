686fbc19b3cf2f8dbcc26af5619dc66e17a81807
yanasan <ptomm08966@yahoo.co.jp>
2025-05-02 17:31:15 +0900

    update 2025/05/02/1

---
+++ practices/polynomial.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "polynomial.h"
+
+
+Complex output(Complex z, void *params)
+{
+    int i;
+    Complex w = {0, 0};
+    Polynomial *poly = (Polynomial*)params;
+    for (i = 0; i < poly->deg + 1; i++) {
+        w = complex_add(w, complex_times(poly->coefficients[i], complex_power(z, i)));
+    }
+    return w;
+}
+
+Polynomial poly_add(Polynomial p, Polynomial q)
+{
+    Focuspolynomial fp;
+    Polynomial r;
+    fp = max_degree(p, q);
+    r.deg = fp.deg;
+    r.coefficients = malloc(sizeof(Complex) * (r.deg + 1));
+    int i;
+    for (i = 0; i < r.deg + 1; i++) {
+        if (i < r.deg) {
+            r.coefficients[i] = complex_add(p.coefficients[i], q.coefficients[i]);
+        } else {
+            r.coefficients[i] = fp.poly.coefficients[i];
+        }
+    }
+    return r;
+}
+
+Polynomial poly_minus(Polynomial p, Polynomial q)
+{
+    Focuspolynomial fp;
+    Polynomial r;
+    fp = max_degree(p, q);
+    r.deg = fp.deg;
+    r.coefficients = malloc(sizeof(Complex) * (r.deg + 1));
+    int i;
+    for (i = 0; i < fp.deg + 1; i++) {
+        if (i < fp.deg) {
+            r.coefficients[i] = complex_add(p.coefficients[i], q.coefficients[i]);
+        } else {
+            r.coefficients[i] = fp.poly.coefficients[i];
+        }
+    }
+    return r;
+}
+
+Polynomial poly_times(Polynomial p, Polynomial q)
+{
+    Polynomial r;
+    r.deg = p.deg + q.deg;
+    r.coefficients = malloc(sizeof(Complex) * (r.deg + 1));
+    Complex temp = {0, 0};
+    int i, j;
+    Focuspolynomial fp1 = min_degree(p, q);
+    Focuspolynomial fp2 = max_degree(p, q);
+    for (i = 0; i < r.deg + 1; i++) {
+        temp = (Complex){0, 0};
+        for (j = 0; j < fp1.deg + 1 && j <= i; j++) {
+            if (i - j < fp2.deg + 1) {
+                temp = complex_add(temp, complex_times(fp1.poly.coefficients[j], fp2.poly.coefficients[i - j]));
+            }
+        }
+        r.coefficients[i] = temp;
+    }
+    return r;
+}
+
+Polynomial poly_div(Polynomial p, Polynomial q)
+{
+    if (p.deg < q.deg) {
+        Polynomial r;
+        r.deg = 0;
+        Complex* c = malloc(sizeof(Complex) * (r.deg + 1));
+        c[0] = (Complex){0, 0};
+        r.coefficients = c;
+        return r;
+    } else {
+        Polynomial r;
+        r.deg = p.deg - q.deg;
+        r.coefficients = malloc(sizeof(Complex) * (r.deg + 1));
+        Polynomial P, Q, R;
+        P = p;
+        Q = q;
+        int i;
+        int count = 0;
+        Complex temp;
+        while (P.deg >= Q.deg) {
+            r.coefficients[r.deg - count] = complex_div(P.coefficients[P.deg], Q.coefficients[Q.deg]);
+            for (i = 1; i < Q.deg + 1; i++) {
+                temp = complex_minus(P.coefficients[r.deg - i], complex_times(complex_div(P.coefficients[P.deg], Q.coefficients[Q.deg]), Q.coefficients[Q.deg - i]));
+                if (temp.x == 0 && temp.y == 0) {
+                    count += 1;
+                    r.coefficients[r.deg - count] = (Complex){0, 0};
+                } else {
+                    break;
+                }
+            }
+            R.deg = P.deg - i;
+            R.coefficients = malloc(sizeof(Complex) * (R.deg + 1));
+            for (i = 0; i < R.deg + 1; i++) {
+                if (i < P.deg - Q.deg) {
+                    R.coefficients[i] = P.coefficients[i];
+                } else {
+                    R.coefficients[i] = complex_minus(P.coefficients[i], complex_times(complex_div(P.coefficients[P.deg], Q.coefficients[Q.deg]), Q.coefficients[i- P.deg + Q.deg]));
+                }
+            }
+            count += 1;
+            P = R;
+        }
+        return r;
+    }
+}
+
+Polynomial poly_remainder(Polynomial p, Polynomial q)
+{
+    if (p.deg < q.deg) {
+        return q;
+    } else {
+        int deg = p.deg - q.deg;
+        Polynomial P, Q, R;
+        P = p;
+        Q = q;
+        int i;
+        int count = 0;
+        Complex temp;
+        while (P.deg >= Q.deg) {
+            for (i = 1; i < Q.deg + 1; i++) {
+                temp = complex_minus(P.coefficients[deg - i], complex_times(complex_div(P.coefficients[P.deg], Q.coefficients[Q.deg]), Q.coefficients[Q.deg - i]));
+                if (temp.x != 0 || temp.y != 0) {
+                    break;
+                }
+            }
+            R.deg = P.deg - i;
+            R.coefficients = malloc(sizeof(Complex) * (R.deg + 1));
+            for (i = 0; i < R.deg + 1; i++) {
+                if (i < P.deg - Q.deg) {
+                    R.coefficients[i] = P.coefficients[i];
+                } else {
+                    R.coefficients[i] = complex_minus(P.coefficients[i], complex_times(complex_div(P.coefficients[P.deg], Q.coefficients[Q.deg]), Q.coefficients[i- P.deg + Q.deg]));
+                }
+            }
+            count += 1;
+            P = R;
+        }
+        return R;
+    }
+}
+
+Focuspolynomial max_degree(Polynomial p, Polynomial q)
+{
+    Focuspolynomial fp;
+    if (p.deg < q.deg) {
+        fp.deg = q.deg;
+        fp.poly = q;
+    } else {
+        fp.deg = p.deg;
+        fp.poly = p;
+    }
+    return fp;
+}
+
+Focuspolynomial min_degree(Polynomial p, Polynomial q)
+{
+    Focuspolynomial fp;
+    if (p.deg < q.deg) {
+        fp.deg = p.deg;
+        fp.poly = p;
+    } else {
+        fp.deg = q.deg;
+        fp.poly = q;
+    }
+    return fp;
+}
+
+void show_polynomial(Polynomial p)
+{
+    int i;
+    for (i = p.deg; 0 <= i; i--) {
+        if (i != 0) {
+            if (p.coefficients[i].y < 0) {
+                printf("(%f-i%f)x**%d + ", p.coefficients[i].x, -p.coefficients[i].y, i);
+            } else {
+                printf("(%f+i%f)x**%d + ", p.coefficients[i].x, p.coefficients[i].y, i);
+            }
+        } else {
+            if (p.coefficients[i].y < 0) {
+                printf("(%f-i%f)\n", p.coefficients[i].x, -p.coefficients[i].y);
+            } else {
+                printf("%f+i%f\n", p.coefficients[i].x, p.coefficients[i].y);
+            }
+        }
+    }
+} 
---
+++ practices/polynomial.h
@@ -0,0 +1,27 @@
+#ifndef POLYNOMIAL_H_
+#define POLYNOMIAL_H_
+
+#include <stdlib.h>
+#include "complex.h"
+
+typedef struct Polynomial {
+    int deg;
+    Complex* coefficients;
+} Polynomial;
+
+typedef struct Focuspolynomial {
+    int deg;
+    Polynomial poly;
+} Focuspolynomial;
+
+Complex output(Complex z, void *params);
+Polynomial poly_add(Polynomial p, Polynomial q);
+Polynomial poly_minus(Polynomial p, Polynomial q);
+Polynomial poly_times(Polynomial p, Polynomial q);
+Polynomial Euclid(Polynomial p, Polynomial q);
+Polynomial poly_div(Polynomial p, Polynomial q);
+Polynomial poly_remainder(Polynomial p, Polynomial q);
+Focuspolynomial max_degree(Polynomial p, Polynomial q);
+Focuspolynomial min_degree(Polynomial p, Polynomial q);
+void show_polynomial(Polynomial p);
+#endif
